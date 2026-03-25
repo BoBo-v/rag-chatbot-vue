eda3891c48e6c6f7627b98802e4c4efe1f0a97d3
Bobo🇨🇳 <email@sample.com>
2026-03-25 17:00:46 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // 监听所有地址，包括局域网 IP
+        // open: true,   // 运行时是否自动在浏览器打开（可选）
+    },
   plugins: [vue()],
 })
